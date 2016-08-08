15bf03956490e96525765619ada2fed84dd5bde4
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-08-08 21:58:31 +0200

    PROTOTYPE simple ECB rate retrieve with sanity check and naive XML parser

---
+++ prototype_reference/get_ecb_rate.sh
@@ -0,0 +1,69 @@
+#!/bin/bash  
+
+online() {
+  nc -z 8.8.8.8 53 >/dev/null 2>&1; [ $? -eq 0 ]
+}
+
+request() {
+  res=$(curl -sw "%{http_code}" $1)
+  http_code="${res:${#res}-3}"
+
+  if [[ "$http_code" == "200" ]]; then
+    echo "${res:0:${#res}-3}"
+    return 0
+  else
+    return 1
+  fi
+}
+
+read_dom () {
+  local IFS=\>
+  read -d \< ENTITY CONTENT
+  local RET=$?
+  TAG_NAME=${ENTITY%% *}
+  ATTRIBUTES=${ENTITY#* }
+  ATTRIBUTES=${ATTRIBUTES%/}
+  #target=
+
+  return $RET
+}
+
+syncDate="2016-08-08"
+
+parse_dom () {
+  if [[ $TAG_NAME = "Cube" ]] ; then
+    #echo $ATTRIBUTES
+
+    eval local $ATTRIBUTES
+    #echo "$time"
+    if [[ $time ]]; then
+      if [[ ! "$time" == "$syncDate" ]]; then
+        echo "no data for $syncDate"
+        exit 1
+      fi
+    elif [[ $currency && $rate ]]; then
+      echo "1 EUR = $rate $currency"
+    fi
+  fi
+}
+
+if online; then
+  response=$(request "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml")
+  if [ $? -eq 0 ]; then
+    #check=$(head -n 1 <<< "$response")
+    #dataDate=$(cut -d " " -f 1 <<< $check)
+    #dataId=$(cut -d "#" -f 2 <<< $check)
+
+    while read_dom; do
+      parse_dom
+    done  <<< "$response"
+
+    exit 0
+  else
+    echo "network unreachable, bailing out"
+    exit 1
+  fi
+else
+  echo "network offline, nothing to do"
+fi
+
