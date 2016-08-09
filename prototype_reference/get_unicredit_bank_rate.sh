d646a4810ee39e5550cb94f34bd292abea88c0ab
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-08-09 19:42:23 +0200

    PROTOTYPE cookie spoof for unicredit SAMEORIGIN rate scrap

--- prototype_reference/get_unicredit_bank_rate.sh
+++ prototype_reference/get_unicredit_bank_rate.sh
@@ -4,8 +4,19 @@ online() {
   nc -z 8.8.8.8 53 >/dev/null 2>&1; [ $? -eq 0 ]
 }
 
+get_cookie() {
+  res=$(curl -IL -sw "%{http_code}" $1)
+  http_code="${res:${#res}-3}"
+  if [[ "$http_code" == "200" ]]; then
+    grep -o '^Set-Cookie: .*' <<< "${res:0:${#res}-3}"| perl -pne 's/Set-Cookie: (.*)?/$1/g' | cut -d ";" -f 1
+    return 0
+  else
+    return 1
+  fi
+}
+
 request() {
-  res=$(curl -sw "%{http_code}" $1)
+  res=$(curl --cookie "${2}" -sw "%{http_code}" $1)
   http_code="${res:${#res}-3}"
 
   if [[ "$http_code" == "200" ]]; then
@@ -19,10 +30,11 @@ request() {
 syncDate="2016-08-09"
 
 if online; then
-  response=$(request "https://www.unicreditbank.cz/cs/exchange_rates_xml.exportxml.html")
+  # fixme somewhat cleaner and more universal, lot of hardcoding there
+  cookies=$(get_cookie "https://www.unicreditbank.cz/cs/obcane.html")
+  response=$(request "https://www.unicreditbank.cz/cs/exchange_rates_xml.exportxml.html" ${cookies})
 
   if [ $? -eq 0 ]; then
-    # need to retrieve cookie and session first, otherwise returns empty XML
     echo "$response"
     exit 0
   else
