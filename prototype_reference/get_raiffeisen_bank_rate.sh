3430822c5b8c05c6446d38a1ac444a11f20fcc31
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-08-09 10:30:20 +0200

    PROTOTYPE added internal instances retrieval from RB to retrieve rates

--- prototype_reference/get_raiffeisen_bank_rate.sh
+++ prototype_reference/get_raiffeisen_bank_rate.sh
@@ -16,15 +16,22 @@ request() {
   fi
 }
 
-#calc() { awk "BEGIN{print $*}"; }
-
-syncDate="08.08.2016"
+syncDate="09.08.2016"
 
 if online; then
-  urlEncodedDate=$(sed "s/\./\.%20/g" <<< $syncDate)
-  response=$(request "https://www.rb.cz/cs/informacni-servis/informacni-a-online-sluzby/kurzovni-listek?action=xml&date=${urlEncodedDate}")
+  urlEncodedDate=$(sed "s/\./\.+/g" <<< $syncDate)
 
-  # info wont work just like that
+  spoil=$(request "https://www.rb.cz/informacni-servis/informacni-a-online-sluzby/kurzovni-listek")
+  ids=$(grep -o '"[a-zA-Z_]*_INSTANCE_[^"]*' <<< $spoil | perl -pne 's/"(.*)_INSTANCE_([0-9a-z]+)_?/$1: $2/g')
+  DynamicNestedPortlet_id=$(grep -o '"*DynamicNestedPortlet[^"]*' <<< "$ids" | head -1 | cut -d " " -f 2)
+  CurrencyRate_WAR_CurrencyRateportlet_id=$(grep -o '"*CurrencyRate_WAR_CurrencyRateportlet[^"]*'  <<< "$ids" | head -1 | cut -d " " -f 2)
+
+  p_p_id="CurrencyRate_WAR_CurrencyRateportlet_INSTANCE_${CurrencyRate_WAR_CurrencyRateportlet_id}"
+  p_p_col_id="_DynamicNestedPortlet_INSTANCE_${DynamicNestedPortlet_id}__column-1"
+  p_p_out="_CurrencyRate_WAR_CurrencyRateportlet_INSTANCE_${CurrencyRate_WAR_CurrencyRateportlet_id}_action"
+  p_p_date="_CurrencyRate_WAR_CurrencyRateportlet_INSTANCE_${CurrencyRate_WAR_CurrencyRateportlet_id}_date"
+
+  response=$(request "https://www.rb.cz/cs/informacni-servis/informacni-a-online-sluzby/kurzovni-listek?p_p_id=${p_p_id}&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_col_id=$p_p_col_id&p_p_col_count=1&${p_p_date}=${urlEncodedDate}&${p_p_out}=xml")
 
   if [ $? -eq 0 ]; then
     echo "$response"
@@ -36,4 +43,3 @@ if online; then
 else
   echo "network offline, nothing to do"
 fi
-
