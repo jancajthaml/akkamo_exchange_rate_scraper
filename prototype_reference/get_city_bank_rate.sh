04b6acec0b131a719d830b9b0f54b8cca7885bb3
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-08-16 13:26:53 +0200

    PROTOTYPE added not implemented message to city bank to remove confusion

--- prototype_reference/get_city_bank_rate.sh
+++ prototype_reference/get_city_bank_rate.sh
@@ -5,16 +5,18 @@
 syncDate="2016-8-11"
 
 if online; then
-  response=$(request "https://www.airbank.cz/cs/kurzovni-listek/")
-  if [ $? -eq 0 ]; then
-    currencies_table=$(tr -d '\011\012\015' <<< $response | awk -v FS="(<div class='contentAjax'>|</table>)" '{print $2}' | awk -v FS="(<tbody>|</tbody>)" '{print $2}')
+  echo "!!! not implemented !!!"
+  exit 1
+  #response=$(request "https://www.airbank.cz/cs/kurzovni-listek/")
+  #if [ $? -eq 0 ]; then
+    #currencies_table=$(tr -d '\011\012\015' <<< $response | awk -v FS="(<div class='contentAjax'>|</table>)" '{print $2}' | awk -v FS="(<tbody>|</tbody>)" '{print $2}')
 
-    echo "<frag>${currencies_table}</frag>"
-    exit 0
-  else
-    echo "network unreachable, bailing out"
-    exit 1
-  fi
+   # echo "<frag>${currencies_table}</frag>"
+    #exit 0
+  #else
+   # echo "network unreachable, bailing out"
+    #exit 1
+  #fi
 else
   echo "network offline, nothing to do"
 fi
