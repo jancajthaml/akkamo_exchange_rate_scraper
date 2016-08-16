03757442ed891f712e15c4f044b08d7fd0e37a7b
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-08-16 11:29:17 +0200

    PROTOTYPE parsing air bank scrap with XPATH

--- prototype_reference/get_air_bank_rate.sh
+++ prototype_reference/get_air_bank_rate.sh
@@ -22,8 +22,30 @@ if online; then
   response=$(request "https://www.airbank.cz/cs/kurzovni-listek/")
   if [ $? -eq 0 ]; then
     currencies_table=$(tr -d '\011\012\015' <<< $response | awk -v FS="(<div class='contentAjax'>|</table>)" '{print $2}' | awk -v FS="(<tbody>|</tbody>)" '{print $2}')
+    data="<frag>${currencies_table}</frag>"
 
-    echo "<frag>${currencies_table}</frag>"
+    #xpath is extremely slow in this case, change to sed or awk in future
+    lines=$(xpath 'count(//frag/tr)' <<< $data 2> /dev/null)
+
+    for ((i=0; i<${lines}; i++)); do
+      row=$(xpath "(//frag/tr)[${i}]" <<< $data 2> /dev/null)
+      currency=$(xpath "//tr/td[contains(@class,'code')]/strong/text()" <<< $row 2> /dev/null)
+      
+      if [ ! -z $currency ]; then
+        amount=$(xpath "//tr/td[contains(@class,'amount')]/text()" <<< $row 2> /dev/null)
+        sell=$(xpath "//tr/td[contains(@class,'sell')]/text()" <<< $row 2> /dev/null)
+        buy=$(xpath "//tr/td[contains(@class,'buy')]/text()" <<< $row 2> /dev/null)
+
+        sell=${sell//[,]/.}
+        buy=${buy//[,]/.}
+
+        normalizedBuy=$(lua -e "print($sell/$amount)")
+        normalizedSell=$(lua -e "print($buy/$amount)")
+        
+        echo "1 $currency = sell: $normalizedSell CZK, buy: $normalizedBuy CZK"
+      fi
+
+    done
 
     exit 0
   else
