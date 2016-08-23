8e7c64d621b0ee0a15301a042a372682641e2b51
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-08-23 10:33:06 +0200

    PROTOTYPE implemented parsing of expobank resources

--- prototype_reference/get_expo_bank_rate.sh
+++ prototype_reference/get_expo_bank_rate.sh
@@ -2,7 +2,7 @@
 
 . common.sh
 
-syncDate="2016-08-02"
+syncDate="2016-08-22"
 
 if online; then
   responseDeviza=$(request "https://www.expobank.cz/fis/rs/sp/public/web/exchangerate/MARKET/${syncDate}?get_param=value")
@@ -12,8 +12,91 @@ if online; then
 
     if [ $? -eq 0 ]; then
 
-      echo "$responseDeviza"
-      echo "$responseValuta"
+      lines=$(python -c "
+from __future__ import print_function
+import os
+import json
+import sys
+import traceback
+syncDate='${syncDate}'
+valutas=${responseValuta}
+devizas=${responseDeviza}
+merge={}
+try:
+  for valuta in valutas:
+    if syncDate == valuta['dateCreated'].split('T')[0]:
+      merge[valuta['currency']] = {
+        'buyValuta': valuta['buyRate'],
+        'sellValuta': valuta['saleRate'],
+        'amountValuta': valuta['quotationUnit']
+      }
+  for deviza in devizas:
+    if syncDate == deviza['dateCreated'].split('T')[0]:
+
+      if deviza['currency'] in merge:
+        merge[deviza['currency']]['buyDeviza'] = deviza['buyRate']
+        merge[deviza['currency']]['sellDeviza'] = deviza['saleRate']
+        merge[deviza['currency']]['amountDeviza'] = deviza['quotationUnit']
+      else:
+        merge[deviza['currency']] = {
+          'buyDeviza': deviza['buyRate'],
+          'sellDeviza': deviza['saleRate'],
+          'amountDeviza': deviza['quotationUnit']
+        }
+  for currency, data in merge.iteritems():
+    print('{0} {1} {2} {3} {4} {5} {6}'.format(
+      currency,
+      data.get('amountDeviza', '-'),
+      data.get('buyDeviza', '-'),
+      data.get('sellDeviza', '-'),
+      data.get('amountValuta', '-'),
+      data.get('buyValuta', '-'),
+      data.get('sellValuta', '-')
+    ))
+    
+except Exception:
+  exc_type, exc_value, exc_traceback = sys.exc_info()
+  print(exc_value)
+  print('\n'.join(traceback.format_tb(exc_traceback)))
+  sys.exit(1)
+") 
+
+      while IFS='' read -r row; do
+        IFS=' ' read -r -a args <<< "$row"
+
+        currencyTarget=${args[0]}
+        currencySource="CZK"
+
+        normalizedAmount="1"
+
+        #devizy nakup/prodej
+        amountDeviza=${args[1]}
+        normalizedBuyDeviza=$(calculate "${args[2]} / ${amountDeviza}")
+        normalizedSellDeviza=$(calculate "${args[3]} / ${amountDeviza}")
+
+        #valuty nakup/prodej
+        amountValuta=${args[4]}
+        buyValuta=${args[5]}
+        sellValuta=${args[6]}
+
+        if [[ "$amountValuta" == "-" || "$buyValuta" == "-" || "$sellValuta" == "-" ]]; then
+          echo "$normalizedAmount $currencyTarget >> VIRTUAL_RATE { sell: $normalizedSellDeviza $currencySource, buy: $normalizedBuyDeviza $currencySource }, DATE: { $syncDate }"
+        else
+          normalizedBuyValuta=$(calculate "$buyValuta / $amountValuta")
+          normalizedSellValuta=$(calculate "$sellValuta / $amountValuta")
+
+          echo "$normalizedAmount $currencyTarget >> VIRTUAL_RATE { sell: $normalizedSellDeviza $currencySource, buy: $normalizedBuyDeviza $currencySource }, CASH_RATE { sell: $normalizedSellValuta $currencySource, buy: $normalizedBuyValuta $currencySource }, DATE: { $syncDate }"
+        fi
+
+        #amount=${args[1]}
+
+        #normalizedBuyDeviza=$(calculate "${args[3]} / $amount")
+        #normalizedSellDeviza=$(calculate "${args[2]} / $amount")
+
+        #normalizedAmount="1"
+
+        #echo "$normalizedAmount $currencyTarget >> VIRTUAL_RATE { sell: $normalizedSellDeviza $currencySource, buy: $normalizedBuyDeviza $currencySource }, DATE: { $syncDate }"
+      done <<< "$lines"
 
       exit 0
     else
