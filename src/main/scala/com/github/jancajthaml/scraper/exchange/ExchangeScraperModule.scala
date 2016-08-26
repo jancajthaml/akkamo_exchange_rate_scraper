35b4b17a335c0ff9e1987c0c44a1540cd179139c
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-08-26 13:14:43 +0200

    added CNB Rate skeleton

--- src/main/scala/com/github/jancajthaml/scraper/exchange/ExchangeScraperModule.scala
+++ src/main/scala/com/github/jancajthaml/scraper/exchange/ExchangeScraperModule.scala
@@ -14,6 +14,7 @@ class ExchangeScraperModule extends akkamo.Module with akkamo.Initializable {
   override def dependencies(dependencies: Dependency): Dependency = dependencies
     .&&[akkamo.LogModule]
 
+
   override def initialize(ctx: Context): Res[Context] = Try {
     // inject dependencies
     val log: LoggingAdapter = ctx.get[LoggingAdapterFactory] apply getClass
---
+++ src/main/scala/com/github/jancajthaml/scraper/utils/GetCNBRate.scala
@@ -0,0 +1,9 @@
+package com.github.jancajthaml.scraper.utils
+
+/*
+
+//CSV based response
+
+https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt?date=12.08.2016
+
+*/
